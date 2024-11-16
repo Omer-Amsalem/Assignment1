7c8aafc1fed0b4ea8d3873f88ff0e1c241a0fec6
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2024-11-16 12:49:29 +0200

    Build express server and connection to Mongo DB

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Initialize Express
+const app = express();
+
+// Middleware
+app.use(express.json()); // To parse JSON
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.error('MongoDB connection failed:', err));
+
+// Routes
+//app.use('/api/items', require('./routes/items'));
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
